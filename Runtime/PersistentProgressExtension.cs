2bcfbdd8124f66a5cfda8149b0b30ade59c144cd
avmedvedskiy <a.v.medvedskiy@gmail.com>
2024-09-24 16:59:56 +0300

    disable thread pool on web

--- Runtime/PersistentProgressExtension.cs
+++ Runtime/PersistentProgressExtension.cs
@@ -7,12 +7,20 @@ namespace StorageSystem
     {
         public static async UniTask WriteOnThreadPool(this IPersistentProgressStorage storage)
         {
+#if UNITY_WEBGL
+            await storage.WriteSave();
+#else
             await UniTask.RunOnThreadPool(storage.WriteSave);
+#endif
         }
         
         public static async UniTask ReadOnThreadPool(this IPersistentProgressStorage storage)
         {
+#if UNITY_WEBGL
+            await storage.ReadSave();
+#else
             await UniTask.RunOnThreadPool(storage.ReadSave);
+#endif
         }
     }
 }
